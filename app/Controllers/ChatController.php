11894f0d67fae2758f52bd5a17bd22816686065d
Dilip <dilip7844@gmail.com>
2022-08-27 15:37:24 +0530

    cpanel yml update

--- app/Controllers/ChatController.php
+++ app/Controllers/ChatController.php
@@ -1,20 +1,21 @@
 <?php
-$host="app.pachal.in";
-//$host="127.0.0.1";
+//$host="app.pachal.in";
+$host="localhost";
 $port=8085;
 $socket=socket_create(AF_INET,SOCK_STREAM,0) or die('Not Created');
 $result=socket_bind($socket,$host,$port) or die('Not bind');
 $result=socket_listen($socket,3) or die('Not listen');
+//echo $result;
 
 do{
 $accept=socket_accept($socket) or die('Not accept');
 $msg=socket_read($accept,1024);
 $msg=trim($msg);
-echo $msg."\n";
+echo "\nClient Reply : ".$msg."\n\n";
 
-echo "Enter reply";
-$reply=fgets(STDIN);
-socket_write($accept,$reply,strlen($reply));
+//echo "Enter reply";
+//$reply=fgets(STDIN);
+socket_write($accept,$msg,strlen($msg));
 }while(true);
 
 socket_close($socket);
